2a2ebc10dadea8b90e5dc9729977ace6fcdf416d
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-06-26 23:26:16 -0300

    remodelação do codigo

--- src/Principal.java
+++ src/Principal.java
@@ -1,85 +1,97 @@
 import java.io.IOException;
 import java.util.InputMismatchException;
-import java.util.Objects;
 import java.util.Scanner;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        ExchangerateApi exchangerateApi = new ExchangerateApi();
+        int option = 0;
 
-        Scanner leitura = new Scanner(System.in);
-        ExchangerateApi cunsulta = new ExchangerateApi();
-        double valor;
-        int opcao = 0;
-        String msg = "Digite o valor que irá converter: ";
+        while (option != 7) {
+            printMenu();
+            option = getOption(scanner);
 
-        try {
-            while (opcao != 7) {
-
-
-            System.out.println("""
-                     ****************************************************
-                     | Bem-vindo ao conversor de moedas:                |
-                     |                                                  |
-                     | 1) Dolar => Real                                 |
-                     | 2) Real => Dolar                                 |
-                     | 3) Dolar => Euro                                 |
-                     | 4) Euro => Dolar                                 |
-                     | 5) Dolar => Libra esterlina                      |
-                     | 6) Libra esterlina => Dolar                      |
-                     | 7) Sair                                          |
-                     |                                                  |
-                     ****************************************************
-                    """);
-
-
-                System.out.println("Selecione uma opção: ");
-                opcao = leitura.nextInt();
-
-                if (Objects.equals(opcao, 1)) {
-                    Conversor conversor = cunsulta.ObtemMoeda("USD", "BRL");
-                    System.out.println(msg);
-                    valor = leitura.nextDouble();
-                    System.out.printf("Valor %.2f [USD] Corresponde a ==> " + conversor.conversion_rate() * valor + "[BRL]%n", valor);
-                } else if (Objects.equals(opcao, 2)) {
-                    Conversor conversor = cunsulta.ObtemMoeda("BRL", "USD");
-                    System.out.println(msg);
-                    valor = leitura.nextDouble();
-                    System.out.printf("Valor %.2f [BRL] Corresponde a ==> " + conversor.conversion_rate() * valor + "[USD]%n", valor);
-                } else if (Objects.equals(opcao, 3)) {
-                    Conversor conversor = cunsulta.ObtemMoeda("USD", "EUR");
-                    System.out.println(msg);
-                    valor = leitura.nextDouble();
-                    System.out.printf("Valor %.2f [USD] Corresponde a ==> " + conversor.conversion_rate() * valor + "[EUR]%n", valor);
-                } else if (Objects.equals(opcao, 4)) {
-                    Conversor conversor = cunsulta.ObtemMoeda("EUR", "USD");
-                    System.out.println(msg);
-                    valor = leitura.nextDouble();
-                    System.out.printf("Valor %.2f [EUR] Corresponde a ==> " + conversor.conversion_rate() * valor + "[USD]%n", valor);
-                } else if (Objects.equals(opcao, 5)) {
-                    Conversor conversor = cunsulta.ObtemMoeda("USD", "GBP");
-                    System.out.println(msg);
-                    valor = leitura.nextDouble();
-                    System.out.printf("Valor %.2f [USD] Corresponde a ==> " + conversor.conversion_rate() * valor + "[GBP]%n", valor);
-                } else if (Objects.equals(opcao, 6)) {
-                    Conversor conversor = cunsulta.ObtemMoeda("GBP", "USD");
-                    System.out.println(msg);
-                    valor = leitura.nextDouble();
-                    System.out.printf("Valor %.2f [GBP] Corresponde a ==> " + conversor.conversion_rate() * valor + "[USD]%n", valor);
-                } else if (Objects.equals(opcao, 7)) {
-                    break;
-                }
-
-                System.out.println("Digite [0] para continuar| [7] para sair");
-                opcao = leitura.nextInt();
+            if (option == 7) {
+                break;
             }
 
-        } catch (Exception e) {
-            System.out.println("Digite somente numeros");
+            try {
+                double value = getValue(scanner, "Digite o valor que irá converter: ");
+                processOption(option, value, exchangerateApi);
+            } catch (InputMismatchException e) {
+                System.out.println("Digite somente números.");
+                scanner.next();  // Limpar a entrada inválida
+            }
+
+            option = getContinueOption(scanner);
         }
 
         System.out.println("Programa finalizado!");
+    }
+
+    private static void printMenu() {
+        System.out.println("""
+                 ****************************************************
+                 | Bem-vindo ao conversor de moedas:                |
+                 |                                                  |
+                 | 1) Dolar => Real                                 |
+                 | 2) Real => Dolar                                 |
+                 | 3) Dolar => Euro                                 |
+                 | 4) Euro => Dolar                                 |
+                 | 5) Dolar => Libra esterlina                      |
+                 | 6) Libra esterlina => Dolar                      |
+                 | 7) Sair                                          |
+                 |                                                  |
+                 ****************************************************
+                """);
+    }
+
+    private static int getOption(Scanner scanner) {
+        System.out.print("Selecione uma opção: ");
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Opção inválida. Por favor, digite um número entre 1 e 7.");
+            scanner.next();  // Limpar a entrada inválida
+            return 0;
+        }
+    }
+
+    private static double getValue(Scanner scanner, String message) {
+        System.out.print(message);
+        return scanner.nextDouble();
+    }
+
+    private static void processOption(int option, double value, ExchangerateApi exchangerateApi) throws IOException, InterruptedException {
+        String fromCurrency = null;
+        String toCurrency = null;
+
+        switch (option) {
+            case 1 -> { fromCurrency = "USD"; toCurrency = "BRL"; }
+            case 2 -> { fromCurrency = "BRL"; toCurrency = "USD"; }
+            case 3 -> { fromCurrency = "USD"; toCurrency = "EUR"; }
+            case 4 -> { fromCurrency = "EUR"; toCurrency = "USD"; }
+            case 5 -> { fromCurrency = "USD"; toCurrency = "GBP"; }
+            case 6 -> { fromCurrency = "GBP"; toCurrency = "USD"; }
+            default -> System.out.println("Opção inválida.");
+        }
+
+        if (fromCurrency != null && toCurrency != null) {
+            Conversor conversor = exchangerateApi.ObtemMoeda(fromCurrency, toCurrency);
+            double convertedValue = conversor.conversion_rate() * value;
+            System.out.printf("Valor %.2f [%s] Corresponde a ==> %.2f [%s]%n", value, fromCurrency, convertedValue, toCurrency);
+        }
+    }
+
+    private static int getContinueOption(Scanner scanner) {
+        System.out.print("Digite [0] para continuar | [7] para sair: ");
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Saindo do programa.");
+            scanner.next();  // Limpar a entrada inválida
+            return 7;
+        }
     }
 }
